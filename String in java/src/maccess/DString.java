30633269fccc1556cd8ea2fe8f6e93a60834a9a5
QA12 <QA12@QAPLANET-PC>
2019-08-14 13:31:38 +0530

    pushing to my shaar repository

---
+++ String in java/src/maccess/DString.java
@@ -0,0 +1,10 @@
+package maccess;
+public class DString {
+public static void main(String args[]){
+String s1="java";
+String s2=new String("program");
+System.out.println("---Dis---");
+System.out.println("s1:"+s1.toString());
+System.out.println("s2:"+s2.toString());
+}
+}
